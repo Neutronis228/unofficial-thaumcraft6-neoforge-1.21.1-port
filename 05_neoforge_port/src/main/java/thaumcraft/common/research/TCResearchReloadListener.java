1808b6b59bccd97c49f482c8965a8c79cfe3297a
_zsz_ <jktu12345522@gmail.com>
2026-09-01 10:40:42 +0300

    Add research JSON transform API for addon patchers

--- 05_neoforge_port/src/main/java/thaumcraft/common/research/TCResearchReloadListener.java
+++ 05_neoforge_port/src/main/java/thaumcraft/common/research/TCResearchReloadListener.java
@@ -1,11 +1,13 @@
 package thaumcraft.common.research;
 
 import com.google.gson.JsonElement;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
+import thaumcraft.api.research.TCResearchJsonTransformers;
 
 final class TCResearchReloadListener extends SimpleJsonResourceReloadListener {
     TCResearchReloadListener() {
@@ -14,6 +16,9 @@ final class TCResearchReloadListener extends SimpleJsonResourceReloadListener {
 
     @Override
     protected void apply(Map<ResourceLocation, JsonElement> files, ResourceManager resourceManager, ProfilerFiller profiler) {
-        TCResearchManager.reload(TCResearchParser.parse(files));
+        LinkedHashMap<ResourceLocation, JsonElement> transformed = new LinkedHashMap<>();
+        files.forEach((id, json) -> transformed.put(id, json.deepCopy()));
+        TCResearchJsonTransformers.apply(transformed, resourceManager);
+        TCResearchManager.reload(TCResearchParser.parse(transformed));
     }
 }
