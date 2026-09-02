7bce708f9fdcdf93c2bdd31c57a5c8cf879db01f
Neutronis228 <neutronteamzsz@gmail.com>
2026-09-02 13:20:04 +0300

    items: expose rechargeable item refill helper

--- 05_neoforge_port/src/main/java/thaumcraft/common/items/TCRechargeHelper.java
+++ 05_neoforge_port/src/main/java/thaumcraft/common/items/TCRechargeHelper.java
@@ -1,7 +1,9 @@
 package thaumcraft.common.items;
 
 import net.minecraft.world.item.ItemStack;
+import thaumcraft.common.items.armor.ItemTravellerBoots;
 import thaumcraft.common.registry.TCDataComponents;
+import thaumcraft.common.registry.TCItems;
 
 /**
  * Small NeoForge data-component replacement for TC6's IRechargable +
@@ -13,6 +15,17 @@ import thaumcraft.common.registry.TCDataComponents;
  */
 public final class TCRechargeHelper {
     private TCRechargeHelper() {
+    }
+
+    public static int getMaxCharge(ItemStack stack) {
+        if (stack.getItem() == TCItems.TRAVELLER_BOOTS.get()) {
+            return ItemTravellerBoots.MAX_CHARGE;
+        }
+        return 0;
+    }
+
+    public static boolean canRecharge(ItemStack stack) {
+        return getMaxCharge(stack) > 0;
     }
 
     public static int getCharge(ItemStack stack) {
@@ -42,6 +55,44 @@ public final class TCRechargeHelper {
         int energy = Math.min(getActiveEnergy(stack), max);
         stack.set(TCDataComponents.CHARGE.get(), charge);
         stack.set(TCDataComponents.ACTIVE_ENERGY.get(), energy);
+    }
+
+    public static int recharge(ItemStack stack, int amount) {
+        int maxCharge = getMaxCharge(stack);
+        if (maxCharge <= 0 || amount <= 0) {
+            return 0;
+        }
+
+        ensureInitialCharge(stack, maxCharge);
+        clampCharge(stack, maxCharge);
+
+        int charge = getCharge(stack);
+        int accepted = Math.min(amount, maxCharge - charge);
+        if (accepted <= 0) {
+            return 0;
+        }
+
+        stack.set(TCDataComponents.CHARGE.get(), charge + accepted);
+        return accepted;
+    }
+
+    public static int discharge(ItemStack stack, int amount) {
+        int maxCharge = getMaxCharge(stack);
+        if (maxCharge <= 0 || amount <= 0) {
+            return 0;
+        }
+
+        ensureInitialCharge(stack, maxCharge);
+        clampCharge(stack, maxCharge);
+
+        int charge = getCharge(stack);
+        int consumed = Math.min(amount, charge);
+        if (consumed <= 0) {
+            return 0;
+        }
+
+        stack.set(TCDataComponents.CHARGE.get(), charge - consumed);
+        return consumed;
     }
 
     /**
